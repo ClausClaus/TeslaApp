f8982b1ea4bc0d3fa64ec6a400e9303cd1559ec4
ClausClaus <weixinla84@gmail.com>
2020-12-05 15:46:35 +0800

    Move style files to a separate folder

--- components/CartItem/index.tsx
+++ components/CartItem/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, View, StyleSheet, ImageBackground } from 'react-native';
+import { Text, View, ImageBackground } from 'react-native';
+import styles from './style';
 
 interface Props {}
 
@@ -19,31 +20,5 @@ const CarItem = (props: Props) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  carContainer: {
-    width: '100%',
-    height: '100%',
-  },
-  titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: '500',
-  },
-  subTitle: {
-    fontSize: 16,
-    color: '#5c5e62',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    position: 'absolute',
-  },
-});
 
 export default CarItem;
