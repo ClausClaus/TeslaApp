5f22961cefc9b81ed1ac9d1875d216c223176b7b
ClausClaus <weixinla84@gmail.com>
2020-12-05 21:44:23 +0800

    create a header component

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import CartList from './components/CartList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CartList />
       <StatusBar style="auto" />
     </View>
